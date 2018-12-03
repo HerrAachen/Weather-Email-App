663525d2bfa3de79c1c82b3f8aa825af90f171ff
Malik Atalla <6626492+HerrAachen@users.noreply.github.com>
2018-12-02 23:26:58 -0800

    Identify lines with temperature forecast

--- src/main/java/WeatherApp.java
+++ src/main/java/WeatherApp.java
@@ -1,18 +1,31 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 
 public class WeatherApp {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ParserConfigurationException, SAXException, XPathExpressionException {
         HttpClient client = HttpClient.newBuilder().build();
         HttpRequest getWeatherHtml = HttpRequest
                 .newBuilder()
                 .uri(URI.create("https://weather.gc.ca/forecast/hourly/bc-74_metric_e.html"))
                 .GET()
                 .build();
-        String weatherNetworkHourlyPageString = client.send(getWeatherHtml, HttpResponse.BodyHandlers.ofString()).body();
-        System.out.println(weatherNetworkHourlyPageString);
+        String weatherGcHourly = client.send(getWeatherHtml, HttpResponse.BodyHandlers.ofString()).body();
+        String[] lines = weatherGcHourly.split("\n");
+        Arrays.stream(lines).forEach(line -> {
+            if (line.contains("<td headers=\"header1\" class=\"text-center\">")) {
+                System.out.println(line);
+            }
+            if (line.contains("<td headers=\"header2\" class=\"text-center\">")) {
+                System.out.println(line);
+            }
+        });
     }
 }
